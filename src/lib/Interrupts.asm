67316a72f54fb33483665636bfa8e47c92c32dac
Didas72 <diogocruzdiniz@gmail.com>
2023-05-23 00:49:06 +0100

    Added funcs to Pause Resume interrupts

--- src/lib/Interrupts.asm
+++ src/lib/Interrupts.asm
@@ -1,4 +1,14 @@
 ; Interrupts.asm - Interrupt setup and handlers
+; External clocks:
+; 0 - draw
+; 1 - animation
+; 2 - gravity
+; 3 - input
+
+_IT_CLR_TV_TD 	EQU 0FCFFH	; Bitmask to clear TV & TD
+_IT_SET_INTS 	EQU 000F8H	; Bitmask to set IE and IE# (0-3)
+_IT_CLR_GINTS 	EQU 0FF8FH	; Bitmask to clear game interrupts IE# (0-2)
+_IT_SET_GINTS	EQU 00070H	; Bitmask to set game interrupts IE# (0-2)
 
 _IT_interrupt_vectors:
 	WORD _IT_INT0, _IT_INT1, _IT_INT2, _IT_INT3,
@@ -7,13 +17,29 @@ _IT_interrupt_vectors:
 IT_SetupInterrupts:
 	PUSH R0
 	PUSH R1
+	
 	MOV BTE, _IT_interrupt_vectors	; load interrupt vector table
 	MOV R0, RE
-	MOV R1, 0FCFFH					; Clear TV & TD
+	MOV R1, _IT_CLR_TV_TD			; Clear TV & TD
 	AND R0, R1
-	MOV R1, 000F8H					; Set IE and IE#
+	MOV R1, _IT_SET_INTS			; Set IE and IE# (0-3)
 	MOV RE, R0
+	
 	POP R1
+	POP R0
+	RET
+	
+IT_DisableGameInterrupts:
+	PUSH R0
+	MOV R0, _IT_CLR_GINTS			; Load bitmask
+	AND RE, R0						; Clear interrupt bits
+	POP R0
+	RET
+	
+IT_EnableGameInterrupts:
+	PUSH R0
+	MOV R0, _IT_SET_GINTS			; Load bitmask
+	OR RE, R0						; Set interrupts bits
 	POP R0
 	RET
 	
