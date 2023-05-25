ace0cfb2d08d136d93b59cabbbd8bf6f311355a7
PeakRead <89812720+PeakRead@users.noreply.github.com>
2023-05-25 11:00:30 +0100

    Fixed it
    bazinga
    
    Co-Authored-By: Diogo Cruz Diniz <50275496+didas72@users.noreply.github.com>

--- src/lib/MediaDrive.asm
+++ src/lib/MediaDrive.asm
@@ -40,7 +40,7 @@ WORD 00007H,00020H
 _MD_Glyphs:
 ;		0	  1	  	2	  	3	  	4	  	5	  	6	  7	  	8	  9	  	A	  	B	  	C	  	D	  E	  	F
 WORD 07B6FH,02492H,073E7H,079E7H,049EDH,079CFH,07BCFH,04927H,07BEFH,049EFH,05BEFH,03AEBH,0624EH,03B6BH,073CFH,013CFH
-_MD_Commands EQU 6000H
+MD_Commands EQU 6000H
 _MD_Memmory EQU 8000H
 _MD_Width EQU 32
 _MD_Height EQU 64
@@ -51,9 +51,9 @@ _MD_Height EQU 64
 MD_InitMedia:
 	PUSH R0
 	MOV R0,1
-	MOV [_MD_Commands + 10H],R0;AutoMov enabled some draw call need this on
-	MOV [_MD_Commands + 02H],R0;clear screen(s)
-	MOV [_MD_Commands + 40H],R0;remove warning
+	MOV [MD_Commands + 10H],R0;AutoMov enabled some draw call need this on
+	MOV [MD_Commands + 02H],R0;clear screen(s)
+	MOV [MD_Commands + 40H],R0;remove warning
 	POP R0
 	RET
 
@@ -61,7 +61,7 @@ MD_InitMedia:
 ;Output nothing
 ;Clear screen
 MD_ClearScreen:
-	MOV [_MD_Commands + 02H],R0 ;clear screen(s)
+	MOV [MD_Commands + 02H],R0 ;clear screen(s)
 	RET
 
 ;Input R0(Color)
@@ -78,7 +78,7 @@ MD_ColorBack:
 	MOV R2,_MD_Height
 	MUL R1,R2 ;Get Total Pixel Count
 _MD_ColorBack_loop:
-	MOV [_MD_Commands + 12H],R0 ;Draw And Move
+	MOV [MD_Commands + 12H],R0 ;Draw And Move
 	SUB R0,1
 	CMP R0,0
 	JNZ _MD_ColorBack_loop
@@ -93,16 +93,16 @@ _MD_ColorBack_loop:
 ;Output nothing
 ;Draws a pixel
 MD_DrawPixel:
-	MOV [_MD_Commands+0CH],R0;Set X
-	MOV [_MD_Commands+0AH],R1;Set Y
-	MOV [_MD_Commands+12H],R2;Draw
+	MOV [MD_Commands+0CH],R0;Set X
+	MOV [MD_Commands+0AH],R1;Set Y
+	MOV [MD_Commands+12H],R2;Draw
 	RET
 
 ;Input R0(ID)
 ;Output nothing
 ;Sets Background
 MD_SetBack:
-	MOV [_MD_Commands+42H],R0 ;Set Background with the adress of MediaCenter plus the command to set background
+	MOV [MD_Commands+42H],R0 ;Set Background with the adress of MediaCenter plus the command to set background
 	RET
 	
 ;Input R0(X) R1(Y) R2(width) R3(height) R4(Color)
@@ -115,12 +115,12 @@ MD_DrawRect:
 	PUSH R5 
 
 _MD_DrawRect_height:
-	MOV [_MD_Commands+0CH],R0;Set X
-	MOV [_MD_Commands+0AH],R1;Set Y
+	MOV [MD_Commands+0CH],R0;Set X
+	MOV [MD_Commands+0AH],R1;Set Y
 	MOV R5,R2
 
 _MD_DrawRect_width:
-	MOV [_MD_Commands+12H],R4;Draw
+	MOV [MD_Commands+12H],R4;Draw
 	SUB R5,1
 	CMP R5,0
 	JNZ _MD_DrawRect_width
@@ -157,8 +157,8 @@ MD_DrawSprite:
 	ADD R2,4;set to the sheet area
 
 _MD_DrawSpriteH_HeightLoop:
-	MOV [_MD_Commands+0CH],R0;Set X
-	MOV [_MD_Commands+0AH],R1;Set Y
+	MOV [MD_Commands+0CH],R0;Set X
+	MOV [MD_Commands+0AH],R1;Set Y
 	MOV R10,R4
 
 _MD_DrawSprite_widthloop:
@@ -170,7 +170,7 @@ _MD_DrawSprite_widthloop:
 	AND R7,R8
 	SHL R7,1
 	MOV R9,[R7+R3]
-	MOV [_MD_Commands+12H],R9;Draw
+	MOV [MD_Commands+12H],R9;Draw
 	SHR R6,4;
 
 	;2 pixel
@@ -179,7 +179,7 @@ _MD_DrawSprite_widthloop:
 	AND R7,R8
 	SHL R7,1
 	MOV R9,[R7+R3]
-	MOV [_MD_Commands+12H],R9;Draw
+	MOV [MD_Commands+12H],R9;Draw
 	SHR R6,4;
 
 	;3 pixel
@@ -188,7 +188,7 @@ _MD_DrawSprite_widthloop:
 	AND R7,R8
 	SHL R7,1
 	MOV R9,[R7+R3]
-	MOV [_MD_Commands+12H],R9;Draw
+	MOV [MD_Commands+12H],R9;Draw
 	SHR R6,4;
 
 	;4 pixel
@@ -197,7 +197,7 @@ _MD_DrawSprite_widthloop:
 	AND R7,R8
 	SHL R7,1
 	MOV R9,[R7+R3]
-	MOV [_MD_Commands+12H],R9;Draw
+	MOV [MD_Commands+12H],R9;Draw
 
 	ADD R2,2
 	SUB R10,1
@@ -239,16 +239,16 @@ MD_DrawHex:
 	MOV R6,0000H
 	MOV R8,5
 _MD_DrawHex_HeightLoop:
-	MOV [_MD_Commands+0CH],R0;Set X
-	MOV [_MD_Commands+0AH],R1;Set Y
+	MOV [MD_Commands+0CH],R0;Set X
+	MOV [MD_Commands+0AH],R1;Set Y
 	MOV R7,3
 _MD_DrawHex_WidthLoop:
 	SHR R4,1
 	JNC _MD_DrawHex_NoCarry
-	MOV [_MD_Commands+12H],R2;Draw Color
+	MOV [MD_Commands+12H],R2;Draw Color
 	JMP _MD_DrawHex_Move
 _MD_DrawHex_NoCarry:
-	MOV [_MD_Commands+12H],R6;Draw nothing
+	MOV [MD_Commands+12H],R6;Draw nothing
 _MD_DrawHex_Move:
 	SUB R7,1
 	CMP R7,0
@@ -271,7 +271,7 @@ _MD_DrawHex_Move:
 ;Output nothing
 ;Plays a video/sound
 MD_Play:
-	MOV [_MD_Commands+5AH],R0
+	MOV [MD_Commands+5AH],R0
 	RET
 
 
@@ -279,7 +279,7 @@ MD_Play:
 ;Output nothing
 ;Plays a video/sound on loop
 MD_Loop:
-	MOV [_MD_Commands+5CH],R0
+	MOV [MD_Commands+5CH],R0
 	RET
 
 
@@ -287,19 +287,19 @@ MD_Loop:
 ;Output nothing
 ;pauses a video/sound
 MD_Pause:
-	MOV [_MD_Commands+5EH],R0
+	MOV [MD_Commands+5EH],R0
 	RET
 
 ;Input R0(ID)
 ;Output nothing
 ;unpauses a video/sound
 MD_Unpause:
-	MOV [_MD_Commands+60H],R0
+	MOV [MD_Commands+60H],R0
 	RET
 
 ;Input R0(ID)
 ;Output nothing
 ;Stops a video/sound
 MD_Stop:
-	MOV [_MD_Commands+66H],R0
+	MOV [MD_Commands+66H],R0
 	RET
